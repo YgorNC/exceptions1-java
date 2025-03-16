8db28ac5aad6f08dc512d95f43640c8df01753bf
Ygor Cortês <horseproject16@gmail.com>
2025-03-16 18:56:45 -0300

    solução muito ruim

---
+++ src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the number of products: ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i <= n; i++) {
+			System.out.println("Product #" + i + " data:");
+			System.out.print("Common, used or imported (c/u/i)?");
+			char ch = sc.next().charAt(0);
+			if (ch == 'c') {
+				System.out.print("Name: ");
+				String name = sc.nextLine();
+				System.out.print("Price: ");
+				double price = sc.nextDouble();
+				System.out.print("Custom fee: ");
+				double custom = sc.nextDouble();
+			}
+		}
+		
+		sc.close();
+	}
+}
---
+++ src/entities/ImportedProduct.java
@@ -0,0 +1,8 @@
+package entities;
+
+public class ImportedProduct {
+
+	public ImportedProduct() {
+		}
+	
+ }
